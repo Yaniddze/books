b633fa4a4eebf6aedbd2067e27e89d19eb43830c
Yaniddze <facecrack1337@gmail.com>
2020-07-17 16:11:02 +0500

    Add token entity

---
+++ backend/BooksApi/Entities/Token.cs
@@ -0,0 +1,14 @@
+using System;
+using BooksApi.Entities.Abstractions;
+
+namespace BooksApi.Entities
+{
+    public class Token: Entity
+    {
+        public string TokenValue { get; set; }
+        public Guid UserId { get; set; }
+        public DateTime ExpiryDate { get; set; }
+        public DateTime CreationDate { get; set; }
+        public string JwtId { get; set; }
+    }
+}
